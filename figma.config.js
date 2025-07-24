2703afcf24bbc82eba8a871b0829ff21660c898b
xujiayu <xujiayu@motern.com>
2025-07-24 12:09:46 +0800

    feat: 去除style，只打包component

--- figma.config.js
+++ figma.config.js
@@ -1,21 +1,8 @@
 // @ts-check
 
 import outputComponentsAsSvg from '@figma-export/output-components-as-svg'
-import outputStylesAsSass from '@figma-export/output-styles-as-sass'
 import transformSvgWithSvgo from '@figma-export/transform-svg-with-svgo'
 
-/** @type { import('@figma-export/types').StylesCommandOptions } */
-const styleOptions = {
-  fileId: process.env.FILE_ID || '',
-  // version: 'xxx123456', // optional - file's version history is only supported on paid Figma plans
-  // ids: ['138:52'], // optional - Export only specified node IDs (the `onlyFromPages` option is always ignored when set)
-  // onlyFromPages: ['icons'], // optional - Figma page names or IDs (all pages when not specified)
-  outputters: [
-    outputStylesAsSass({
-      output: './output'
-    })
-  ]
-}
 
 /** @type { import('@figma-export/types').ComponentsCommandOptions } */
 const componentOptions = {
@@ -50,7 +37,6 @@ const componentOptions = {
 /** @type { import('@figma-export/types').FigmaExportRC } */
 export default {
   commands: [
-    ['styles', styleOptions],
     ['components', componentOptions]
   ]
 }
